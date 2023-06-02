87a1c55715e9d327675e6cade101bddbfac45894
Hari om <hariom@shrinesoft.com>
2023-06-02 12:22:38 +0530

    Deleted features

--- src/main/java/com/example/todoapp/service/TodoService.java
+++ src/main/java/com/example/todoapp/service/TodoService.java
@@ -8,28 +8,44 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TodoService {
     @Autowired
     TodoItemRepo todoItemRepo;
 
-    public List<TodoItem> getList(){
-        List<TodoItem> todoList= new ArrayList<>();
+    public List<TodoItem> getList() {
+        List<TodoItem> todoList = new ArrayList<>();
         todoItemRepo.findAll().forEach(todo1 -> todoList.add(todo1));
         return todoList;
     }
 
-    // method for getting data from pop up and send to server.
+    // method for getting data from pop up and send to server for create new record
 
-    public void newTask(String title, Date createDate, Date dueDate){
-        TodoItem todoItem= new TodoItem();
-        todoItem.setTitle(title);
-        todoItem.setCreateDate(createDate);
-        todoItem.setDueDate(dueDate);
+    public void newTask(TodoItem todoItem) {
         todoItemRepo.save(todoItem);
     }
+
+    // Updating item by Id
+    public void updateTask(Long id, TodoItem updateItem){
+        Optional<TodoItem> todoItemOptional= todoItemRepo.findById(id);
+        if(todoItemOptional.isPresent()){
+            TodoItem todoItem= todoItemOptional.get();
+            todoItem.setTitle(updateItem.getTitle());
+            todoItem.setCreateDate(updateItem.getCreateDate());
+            todoItem.setDueDate(updateItem.getDueDate());
+            todoItemRepo.save(todoItem);
+        }
+        System.out.println("function hitttttttewrewqrrfssfss");
+    }
+
+    // Deleted by Id
+
     public void deleteTask(Long id){
         todoItemRepo.deleteById(id);
     }
+
+
+
 }
